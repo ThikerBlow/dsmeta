f7397d27cfda0fc2ff14d0172228d9b5070d2798
Milena <thiagotavares2005@outlook.com>
2022-11-18 14:47:18 -0300

    Other components

---
+++ backend/src/test/java/com/thikerblow/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thikerblow.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
