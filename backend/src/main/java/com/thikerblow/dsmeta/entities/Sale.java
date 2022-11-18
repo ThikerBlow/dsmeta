c4ffac649f2cb0664a3a055a2d99ee42b57376cf
Milena <thiagotavares2005@outlook.com>
2022-11-18 16:08:25 -0300

    Database

---
+++ backend/src/main/java/com/thikerblow/dsmeta/entities/Sale.java
@@ -0,0 +1,79 @@
+package com.thikerblow.dsmeta.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    public Sale() {
+
+    }
+
+    public Sale(Long id, String sellerName, int visited, int deals, double amount, LocalDate date) {
+        this.id = id;
+        this.sellerName = sellerName;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public int getVisited() {
+        return visited;
+    }
+
+    public void setVisited(int visited) {
+        this.visited = visited;
+    }
+
+    public int getDeals() {
+        return deals;
+    }
+
+    public void setDeals(int deals) {
+        this.deals = deals;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
